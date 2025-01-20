53bcbf3bef63dffd70ea5e41c5ebe65f31d26ddc
Ashbipbinu <abinu@thoughtlinetech.com>
2025-01-21 00:30:07 +0530

    third commit

---
+++ Buy_and_Sell_Stock.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxProfit(self, nums):
+        buy = 0
+        sell = 1
+
+        max_profit = 0
+
+        while sell < len(nums):
+            
+            if nums[buy] < nums[sell]:
+                profit = nums[sell] - nums[buy]
+                max_profit = max(profit, max_profit)
+            else:
+                buy = sell
+            
+            sell = sell + 1
+
+        return max_profit
+
+
+
+sol = Solution()
+
+arr = [7,1,5,3,6,4]
+
+print(sol.maxProfit(arr))
